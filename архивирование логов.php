70f3d63c30efc44d559f9236afc1aefea47f7f77
fanrok <fanrok@mail.ru>
2016-06-01 16:21:28 +0400

    Create архивирование логов.php
    Если функция находит файл с логом в корне, то она создает архив в папке back на сайте с этим файлом, оригинал файла удаляется. Паралельно она проверяет все файлы бекапов в папке back и удаляет те, которым больше недели

---
+++ архивирование логов.php
@@ -0,0 +1,34 @@
+<?
+function archive_delite()//функция ищет лог, архивирует его, удаляет старые архивы с логом
+{
+    require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+    require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/classes/general/tar_gz.php");//подключаем библиотеку для работы с архивами
+    set_time_limit(3600);//архивы большие
+    $dir    = '/home/bitrix/microline.pro/back/';//путь к бекапам
+    $out=false;
+    if (file_exists("/home/bitrix/microline.pro/import_element_log.txt")){
+        $out=true;//если файл лога существует 
+    }
+    if ($out){
+        $files1 = scandir($dir);//получаем список всех заархивированных бекапов
+        $i=1;$MAX_I=9999;//ограничение на бесконечный цикл
+        while((file_exists("/home/bitrix/microline.pro/back/backup_".$i.".tar.gz"))&& ($i < $MAX_I)){//проверсяем все бекапы в паке
+            if (filectime("/home/bitrix/microline.pro/back/backup_".$i.".tar.gz")< (strtotime("now")-604800000)){//если бекапу больше недели удаляем его
+                unlink("/home/bitrix/microline.pro/back/backup_".$i.".tar.gz");
+            }
+            $i++;
+        }
+        if ($i==$MAX_I){
+            $i=1;//если бекапов нету то начнем с начала
+        }
+        $oArchiver = new CArchiver('/home/bitrix/microline.pro/back/backup_'.$i.'.tar.gz', false);//создаем бекап
+        $tres = $oArchiver->add("/home/bitrix/microline.pro/import_element_log.txt");//добавляем туда лог
+        $arErrors = &$oArchiver->GetErrors();
+
+        if(count($arErrors)==0)
+        {
+            unlink("/home/bitrix/microline.pro/import_element_log.txt");//если все прошло успешко удаляем лог
+        }
+    }
+    return "archive_delite();";
+}
